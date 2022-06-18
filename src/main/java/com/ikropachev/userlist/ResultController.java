f9b15d50e186b3778dac1e763df4fe294f77464a
Rock133 <paporotnik796@gmail.com>
2022-06-18 19:18:38 +0300

    add_logging

--- src/main/java/com/ikropachev/userlist/ResultController.java
+++ src/main/java/com/ikropachev/userlist/ResultController.java
@@ -1,17 +1,21 @@
 package com.ikropachev.userlist;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import static com.ikropachev.userlist.UserListUtil.checkUser;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Controller
 public class ResultController {
+    private static final Logger log = getLogger(ResultController.class);
 
     @GetMapping("/result")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="user:empty") String name, Model model) {
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="user:emptyDefault") String name, Model model) {
+        log.info("try to get result with name {}", name);
         String resultMessage = checkUser(name);
         model.addAttribute("resultMessage", resultMessage);
         return "result";
--- src/main/java/com/ikropachev/userlist/UserListUtil.java
+++ src/main/java/com/ikropachev/userlist/UserListUtil.java
@@ -1,12 +1,17 @@
 package com.ikropachev.userlist;
 
 import com.sun.jna.platform.win32.Netapi32Util;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class UserListUtil {
+    private static final Logger log = getLogger(UserListUtil.class);
 
     public static String checkUser(String name) {
         String result = "Пользователя нет";
         Netapi32Util.User[] users = Netapi32Util.getUsers();
+        log.info("try to find user with name {}", name);
         for (Netapi32Util.User user : users) {
             if ((user.name).equals(name)){
                 result = "Пользователь есть";
