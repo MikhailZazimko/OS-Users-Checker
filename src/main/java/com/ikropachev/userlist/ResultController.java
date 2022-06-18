7c545d7a0d31666260d9896f7a5d237d40aeef7a
Rock133 <paporotnik796@gmail.com>
2022-06-18 19:39:48 +0300

    add_swagger

--- src/main/java/com/ikropachev/userlist/ResultController.java
+++ src/main/java/com/ikropachev/userlist/ResultController.java
@@ -1,5 +1,8 @@
 package com.ikropachev.userlist;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,11 +13,14 @@ import static com.ikropachev.userlist.UserListUtil.checkUser;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Controller
+@Tag(name = "Result controller", description = "Get page with result of search")
 public class ResultController {
     private static final Logger log = getLogger(ResultController.class);
 
     @GetMapping("/result")
-    public String getResult(@RequestParam(name = "name", required = false, defaultValue = "user:emptyDefault") String name, Model model) {
+    @Operation(summary = "View result")
+    public String getResult(@RequestParam(name = "name", required = false, defaultValue = "user:emptyDefault")
+                            @Parameter(example = "Администратор", required = true) String name, Model model) {
         log.info("try to get result with name {}", name);
         String resultMessage = checkUser(name);
         model.addAttribute("resultMessage", resultMessage);
