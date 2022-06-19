242be7199a3f04fa4340398419910e2ec46a9e3b
Rock133 <paporotnik796@gmail.com>
2022-06-19 12:19:18 +0300

    fix_autorun_browser_from_app

--- server.bat
+++ server.bat
@@ -2,8 +2,7 @@
 @pause
 @echo Run Spring Boot Application...
 start "userlist" mvn spring-boot:run
-start "" http://localhost:8080/
-@echo Prepare to terminate process before exit
+@echo Next step will terminate process before exit
 @pause
 taskkill /FI "WINDOWTITLE eq userlist - mvn   spring-boot:run"
 @pause
--- src/main/java/com/ikropachev/userlist/UserlistApplication.java
+++ src/main/java/com/ikropachev/userlist/UserlistApplication.java
@@ -3,10 +3,18 @@ package com.ikropachev.userlist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class UserlistApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(UserlistApplication.class, args);
+        openHomePage();
+    }
+
+    private static void openHomePage() throws IOException {
+        Runtime rt = Runtime.getRuntime();
+        rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080");
     }
 }
